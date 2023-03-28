270a53ecfd06ab08e65a5b964ae62128e6d80ac0
AllenJoeG <allen.joeg@gmail.com>
2023-03-28 15:41:02 -0500

    Exploring Routing basics, file structure, comments on routing and linking helper methods

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  # Spins up seven core REST routes for the specified model
+  # index, show, new, edit, create, update, destroy
   resources :cars
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
